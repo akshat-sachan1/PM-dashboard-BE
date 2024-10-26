17209d5d888b8b773e24ba6a3e7ad16c533973dd
Akshat Sachan <156008837+akshat-sachan1@users.noreply.github.com>
2024-10-26 14:08:50 +0530

    :zap: Added cloud PostgreeDB

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,19 @@
 const { Pool } = require('pg');
+const fs = require('fs');
+
+const caCert = fs.readFileSync('src/config/CAcertificate.crt');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+    ca: caCert.toString(),
+  },
 });
 
 const initDb = async () => {
